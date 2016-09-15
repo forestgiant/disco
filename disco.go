63e21144a23b51d55ecc04003ddcc0be23338936
Jesse Lucas <jesse@forestgiant.com>
2016-09-14 21:15:41 -0400

    Converting Disco.members to slice of *node.Node instead of a map.

--- disco.go
+++ disco.go
@@ -18,8 +18,8 @@ import (
 type Disco struct {
 	mu               sync.Mutex
 	multicastAddress string
-	// members          []*node.Node
-	members        map[string]chan struct{}
+	members          []*node.Node
+	// members        map[string]chan struct{}
 	closeChan      chan struct{}   // Returns the monitorRegister goroutine
 	discoveredChan chan *node.Node // node.Serve() sends nodes to this chan
 }
@@ -56,22 +56,22 @@ func NewDisco(multicastAddress string) (*Disco, error) {
 // }
 
 // Deregister takes a node and deregisters it
-// func (d *Disco) deregister(n *node.Node) {
-// 	d.mu.Lock()
-// 	defer d.mu.Unlock()
-// 	// Remove node from regsistered
-// 	for i, m := range d.members {
-// 		// make sure the node we sent matches
-// 		if m == n {
-// 			// remove it from the slice
-// 			d.members = append(d.members[:i], d.members[i+1:]...)
-// 		}
-// 	}
-// }
+func (d *Disco) deregister(n *node.Node) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	// Remove node from regsistered
+	for i, m := range d.members {
+		// make sure the node we sent matches
+		if m == n {
+			// remove it from the slice
+			d.members = append(d.members[:i], d.members[i+1:]...)
+		}
+	}
+}
 
 // Members returns all nodes that are registered
 // TODO update to return Nodes
-func (d *Disco) Members() map[string]chan struct{} {
+func (d *Disco) Members() []*node.Node {
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
@@ -93,7 +93,7 @@ func (d *Disco) Discover(ctx context.Context) (<-chan *node.Node, error) {
 	}
 
 	if d.members == nil {
-		d.members = make(map[string]chan struct{})
+		d.members = []*node.Node{}
 	}
 
 	go func() {
@@ -105,13 +105,14 @@ func (d *Disco) Discover(ctx context.Context) (<-chan *node.Node, error) {
 				dec := gob.NewDecoder(buffer)
 				dec.Decode(rn)
 				rn.SrcIP = resp.SrcIP // set the source address
-				key := rn.String()
-				if d.members[key] == nil {
+				if d.addToMembers(rn) {
 					d.register(results, rn)
 				} else {
-					d.mu.Lock()
-					d.members[key] <- struct{}{}
-					d.mu.Unlock()
+					if index := d.indexOfMember(rn); index != -1 {
+						d.mu.Lock()
+						d.members[index].RegisterChan <- struct{}{}
+						d.mu.Unlock()
+					}
 				}
 			case <-ctx.Done():
 				return
@@ -124,35 +125,30 @@ func (d *Disco) Discover(ctx context.Context) (<-chan *node.Node, error) {
 }
 
 func (d *Disco) register(results chan *node.Node, rn *node.Node) {
-	key := rn.String()
-
 	fmt.Println("registering", rn)
-	registerChan := make(chan struct{})
-
-	d.mu.Lock()
-	d.members[key] = registerChan
-	d.mu.Unlock()
 
 	// If it's new to the members send it as a result
 	rn.Action = node.RegisterAction
+	rn.RegisterChan = make(chan struct{})
+
+	d.mu.Lock()
+	d.members = append(d.members, rn)
+	d.mu.Unlock()
 
 	results <- rn
 
 	go func() {
 		for {
 			t := time.NewTimer(rn.SendInterval * 2)
-
 			select {
-			case <-registerChan:
+			case <-rn.RegisterChan:
 				t.Stop()
 				continue
 			case <-t.C:
 				t.Stop()
 				// Deregister if it times out
 				rn.Action = node.DeregisterAction
-				d.mu.Lock()
-				delete(d.members, rn.String())
-				d.mu.Unlock()
+				d.deregister(rn)
 				fmt.Println("Registration timeout out for", rn, " deregistering")
 				results <- rn
 				return
@@ -162,12 +158,21 @@ func (d *Disco) register(results chan *node.Node, rn *node.Node) {
 }
 
 // Check if the members slice already has the node if it doesn't add it
-// func (d *Disco) addToMembers(n *node.Node) bool {
-// 	for _, m := range d.Members() {
-// 		if m.Equal(n) {
-// 			return false // node is already a member
-// 		}
-// 	}
+func (d *Disco) addToMembers(n *node.Node) bool {
+	for _, m := range d.Members() {
+		if m.Equal(n) {
+			return false // node is already a member
+		}
+	}
 
-// 	return true
-// }
+	return true
+}
+
+func (d *Disco) indexOfMember(n *node.Node) int {
+	for i, a := range d.Members() {
+		if a.Equal(n) {
+			return i
+		}
+	}
+	return -1
+}
